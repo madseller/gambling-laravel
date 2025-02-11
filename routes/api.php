a1e1c889c6a208ff33c859dc814db5901e33cccf
madseller <madseller@yandex.ru>
2025-02-11 11:26:31 +0600

    routes

--- routes/api.php
+++ routes/api.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\NotificationController;
+use App\Http\Controllers\BetController;
 
-Route::post('/send-notification', [NotificationController::class, 'sendNotification']);
+Route::post('/process-bet', [NotificationController::class, 'processBet']);
+Route::post('/place-bet', [BetController::class, 'placeBet']);
